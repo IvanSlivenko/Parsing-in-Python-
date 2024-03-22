9ccdc6e057202b14def7469c8be87db522a4a0ff
IvanSlivenko <ivan8822@ukr.net>
2024-03-22 22:07:23 +0300

    22-03-2024-22-07

---
+++ const.py
@@ -0,0 +1,9 @@
+HOST = 'https://minfin.com.ua'
+URL = 'https://minfin.com.ua/cards/'
+HEADERS = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.112 Safari/537.36'
+}
+
+ACCEPT = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.112 Safari/537.36'
--- parser.py
+++ parser.py
@@ -1,1 +1,47 @@
 import requests
+from bs4 import BeautifulSoup
+import csv
+
+from const import HOST, URL, HEADERS
+
+
+def get_html(url, params=''):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.findAll('div', class_='sc-182gfyr-0 jmBHNg')
+    cards = []
+    count = 0
+    for item in items:
+        count += 1
+        cards.append(
+            {
+
+                'title': item.find('div', class_='be80pr-15 kwXsZB').get_text(),
+                'link_produkt': item.find('div', class_='be80pr-15 kwXsZB').find('a').get('href'),
+
+
+                # 'bank_produkt': item.find('span', class_='be80pr-21 dksWIi').get_text(),
+                # 'produkt_name': item.find('a', class_='cpshbz-0 eRamNS').get_text(),
+
+            }
+        )
+        print(cards)
+    # print(cards)
+    return cards
+
+      # testing
+
+        # curent_item_name = item.find('a', class_='cpshbz-0 eRamNS').get_text()
+        # curent_item_bank = item.find('span',class_='be80pr-21 dksWIi').get_text()
+        # print(f"{count}. Банк {curent_item_bank} - {curent_item_name}")
+    # print(cards)
+    # print(len(cards))
+
+
+
+html = get_html(URL)
+get_content(html.text)
+
